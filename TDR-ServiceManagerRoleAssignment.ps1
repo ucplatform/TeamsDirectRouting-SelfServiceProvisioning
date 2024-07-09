f296eabcbe0ab2a2a8a05edb8226834ea65d218a
ucplatform <138210314+ucplatform@users.noreply.github.com>
2024-07-09 14:38:40 +0100

    Create TDR-ServiceManagerRoleAssignment.ps1

---
+++ TDR-ServiceManagerRoleAssignment.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+    Service Manager RBAC Role Assignment
+
+.DESCRIPTION
+    Assign EA-HALO-ServiceManager the Directory Role - Teams Administrator in Customers Tenant
+    @@@@@ = Variables that include System Names, connection strings or Details that have been removed for saftey reasons 
+    
+.INPUTS
+    Portal Via Webhook
+
+#>
+
+#Graph API to customer tenant to add Ent App to Teams Administrator Directory Role
+$apiUrl = "https://graph.microsoft.com/v1.0/directoryRoles"
+
+
+
+#Graph API to customer tenant to pull Ent App Object ID
+$apiUrl = 'https://graph.microsoft.com/v1.0/servicePrincipals?$search="appId:@@@@@"'
+
+
+
+#Graph API to customer tenant to add Ent App to Teams Administrator Directory Role
+$apiUrl = "https://graph.microsoft.com/v1.0/directoryRoles/$TeamsAdminID/members/`$ref"
