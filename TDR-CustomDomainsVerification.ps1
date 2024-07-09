21d1e094318162ae26e0ea64dc479dbaed23eee3
ucplatform <138210314+ucplatform@users.noreply.github.com>
2024-07-09 14:36:05 +0100

    Create TDR-CustomDomainsVerification.ps1

---
+++ TDR-CustomDomainsVerification.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.SYNOPSIS
+    Phase 2: Domain Verification
+ 
+.DESCRIPTION
+    Verify all custom domains in the Customer Microsoft Tenant
+    @@@@@ = Variables that include System Names, connection strings or Details that have been removed for saftey reasons 
+
+.INPUTS
+    Provisioning Portal Via Webhook
+
+#>
+
+#Verify in customers Microsoft Tenant, will retry while errors
+$apiUrl = "https://graph.microsoft.com/v1.0/domains/$i/verify"
+          
+## Remove TXT records following successful verification in HALO system
+Remove-AzDnsRecordSet -Name $ShortDomain -RecordType TXT -ZoneName @@@@@ -ResourceGroupName "uksouth_dns"
